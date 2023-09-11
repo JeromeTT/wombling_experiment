b813881b07a5827a238505efeb9c06d245d8110e
JeromeTT <jerome5139@gmail.com>
2023-09-11 19:38:32 +1000

    Right menu collapsibility fix

--- js/interface/menu/menu.js
+++ js/interface/menu/menu.js
@@ -1,14 +1,17 @@
 export function menuInitCollapsibleBehaviour() {
   let collapsible = document.getElementById("menu-collapsible");
   let menuContents = document.getElementById("menu-contents");
+  let mapOverlayRight = document.getElementById("map-overlay-right");
   menuContents.style.display = "block"; // initialised to block, so that the if statement in the event listener works correctly
 
   collapsible.addEventListener("click", () => {
     collapsible.classList.toggle("active"); // required for the +/- icons to appear correctly
     if (menuContents.style.display === "block") {
       menuContents.style.display = "none";
+      mapOverlayRight.style.display = "none";
     } else {
       menuContents.style.display = "block";
+      mapOverlayRight.style.display = "block";
     }
   });
   let rightCollapsible = document.getElementById("menu-collapsible-right");
